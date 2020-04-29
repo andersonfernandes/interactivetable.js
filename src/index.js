625d5817a400f58eba26ff5e7d0bb3f9831e430d
Anderson Fernandes <fernandesanderson14@gmail.com>
2020-04-29 11:43:50 -0300

    Add sass structure and config
    - added bootstrap
    
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
-// import _ from 'lodash';
-
-// function component() {
-//   const element = document.createElement('div');
-//
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//
-//   return element;
-// }
-//
-// document.body.appendChild(component());
-
-alert('Alooo');
+import './styles/styles.scss'
+import 'bootstrap'
--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,12 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module:{
+    rules:[
+      {
+        test:/\.(s*)css$/,
+        use:['style-loader','css-loader', 'sass-loader']
+      }
+    ]
+  }
 };
